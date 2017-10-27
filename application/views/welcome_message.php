81f0fa606f99df0a96293f9c5e2d6c60104cbb64
lumagallacio <33137648+lumagallacio@users.noreply.github.com>
2017-10-27 10:24:54 -0200

    paragrafo 1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -75,6 +75,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
+		
+		<p> Sistema APSP 2017</p>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
